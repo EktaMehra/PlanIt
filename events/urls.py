a7fb72cc97bcd8c8eaa69b2890fedfe7a5316aee
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-17 17:38:23 +0000

    Update URL Routing for Events

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('', views.event_list, name='event_list'),
     path('<int:pk>/', views.event_detail, name='event_detail'),
     path('new/', views.event_create, name='event_create'),
