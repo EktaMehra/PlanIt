a23a024d0c63efe7d3ebb2cc45bc27b49e53df73
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-19 15:39:16 +0000

    add bootstrap date function in form

--- events/forms.py
+++ events/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Event
 
 class EventForm(forms.ModelForm):
+    date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 5, 'class': 'form-control'})
+    )
+
     class Meta:
         model = Event
-        fields = ['name', 'description', 'location', 'date', 'time']
+        fields = ['name', 'date', 'description', 'category', 'location', 'time', 'created_by']
--- events/models.py
+++ events/models.py
@@ -5,12 +5,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Event(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
     location = models.CharField(max_length=300)
     date = models.DateField()
     time = models.TimeField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
