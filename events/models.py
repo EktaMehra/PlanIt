6bf4d8159e3d24b5ca7867ec9d343f152526d2cb
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-22 17:56:55 +0000

    add booking function in models.py

--- events/models.py
+++ events/models.py
@@ -15,3 +15,12 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+class Booking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='bookings')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f'{self.name} - {self.event.name}'
