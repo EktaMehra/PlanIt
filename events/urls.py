8a264ddd01ede8789034b0154a8cacfa34b29e4d
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-23 11:04:15 +0000

    update url path to run event detail page for attendees

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('list/', views.event_list, name='event_list'),
-    path('<int:pk>/', views.event_detail, name='event_detail'),
+    path('<int:event_id>/', views.event_detail, name='event_detail'),
     path('create/', views.create_event, name='create_event'),
     path('<int:pk>/edit/', views.event_update, name='event_update'),
     path('<int:pk>/delete/', views.event_delete, name='event_delete'),
