d58deb446a901a2aeb11be8a29b5e4cbd2a1f73b
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-27 11:29:12 +0000

    add test for booking model

--- events/test_models.py
+++ events/test_models.py
@@ -1,9 +1,9 @@
-from .models import Event
+from .models import Event, Booking
 from django.test import TestCase
 from django.contrib.auth.models import User
 
 
-# Test Event
+# Test Event Model
 class EventModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="testuser")
@@ -20,3 +20,29 @@ class EventModelTest(TestCase):
     def test_event_creation(self):
         self.assertEqual(self.event.name, "Test Event")
         self.assertEqual(self.event.created_by, self.user)
+
+
+# Test Booking Model
+class BookingModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+        self.event = Event.objects.create(
+            name="Test Event",
+            date="2025-02-01",
+            description="Test Description",
+            category="Music",
+            location="Test Location",
+            time="18:00:00",
+            created_by=self.user,
+        )
+
+        self.booking = Booking.objects.create(
+            event=self.event,
+            name="John Doe",
+            email="john@example.com",
+        )
+
+    def test_booking_creation(self):
+        self.assertEqual(self.booking.name, "John Doe")
+        self.assertEqual(self.booking.event, self.event)
+
