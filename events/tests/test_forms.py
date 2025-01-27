b1ec75fa4406b05d27e0c22f4a31ac90aa5c3dd3
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-27 11:34:46 +0000

    add Events form test

---
+++ events/tests/test_forms.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from .forms import EventForm 
+from django.contrib.auth.models import User
+
+# Test Events Form
+class EventFormTest(TestCase):
+    def test_event_form_valid(self):
+        user = User.objects.create(username="testuser")
+        form_data = {
+            'name': "Test Event",
+            'date': "2025-02-01",
+            'description': "Test Description",
+            'category': "Music",
+            'location': "Test Location",
+            'time': "18:00:00",
+            'created_by': user.id,
+        }
+        form = EventForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_event_form_invalid(self):
+        form = EventForm(data={})
+        self.assertFalse(form.is_valid())
+
+# Test Booking Form
