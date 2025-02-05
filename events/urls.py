54494548353e2d5817bff5b7f74e887e7f125875
EktaMehra <mehra.ekta95@gmail.com>
2025-02-05 13:44:24 +0000

    rectify booking confirmation path

--- events/urls.py
+++ events/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.event_list, name='event_list'),
+    path('events/', views.event_list, name='event_list'),
     path('<int:id>/details/', views.event_detail, name='event_detail'),
     path('create/', views.create_event, name='create_event'),
     path('<int:id>/edit/', views.event_update, name='event_update'),
     path('<int:id>/delete/', views.event_delete, name='event_delete'),
-    path('<int:id>/confirmation/', views.booking_confirmation, name='booking_confirmation'),
+    path('<int:event_id>/confirmation/', views.booking_confirmation, name='booking_confirmation'),
 ]
