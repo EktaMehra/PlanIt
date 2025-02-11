3ada515326adb05af1dde579679fa42910ffff04
EktaMehra <mehra.ekta95@gmail.com>
2025-02-11 22:18:37 +0000

    Add time specification feature in create event form

--- events/forms.py
+++ events/forms.py
@@ -4,10 +4,13 @@ from .models import Booking, Event
 
 class EventForm(forms.ModelForm):
     date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
     )
     description = forms.CharField(
         widget=forms.Textarea(attrs={'rows': 5, 'class': 'form-control'})
+    )
+    time = forms.TimeField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'form-control', 'placeholder': 'HH:MM'}),
     )
 
     class Meta:
